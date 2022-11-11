2aa1ed4c8fb67a2d9f99281f5ea7613d4932f9df
Andrew <andrewmoren93@gmail.com>
2022-11-11 16:12:00 +0300

    convertor  start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Block } from "./Block";
 import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch("https://63452c19dcae733e8feb2bd7.mockapi.io/curse")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+      })
+      .catch((err) => {
+        alert("Error!");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setToPrice(result);
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    setToPrice(value);
+  };
+
   return (
     <div className="App">
       <Block
-        value={0}
-        currency="RUB"
-        onChangeCurrency={(cur) => console.log(cur)}
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
       />
-      <Block value={0} currency="USD" />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
